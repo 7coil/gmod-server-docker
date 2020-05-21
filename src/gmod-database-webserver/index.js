e658e65c93f8cbfadeebad3c7a946e658585223a
Leondro Lio <github@leondrolio.com>
2020-05-21 20:13:46 +0100

    Fix port, close on sigterm

--- src/gmod-database-webserver/index.js
+++ src/gmod-database-webserver/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dbCon = require('./rethinkdb');
 
 const apiRouter = require('./routes/api/v1')
 
@@ -8,4 +9,10 @@ const app = express();
 app.use(bodyParser.json())
   .use('/api/v1', apiRouter)
 
-app.listen(8000)
+const webServer = app.listen(80)
+
+process.on('SIGTERM', () => {
+  console.log('Stopping Webserver')
+  dbCon.close();
+  webServer.close();
+})
