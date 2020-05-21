ff3397638d7d6492c39ed1260dac2673790e2a77
Leondro Lio <github@leondrolio.com>
2020-05-21 20:46:08 +0100

    Convert req.body.payload to req.payload

--- src/gmod-database-webserver/index.js
+++ src/gmod-database-webserver/index.js
@@ -6,6 +6,18 @@ const apiRouter = require('./routes/api/v1')
 const app = express();
 
 app.use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use((req, res, next) => {
+    if (req.body && typeof req.body.payload === 'object') {
+      req.payload = req.body.payload;
+    } else if (req.body && typeof req.body.payload === 'string') {
+      req.payload = JSON.parse(req.body.payload);
+    } else {
+      req.payload = {};
+    }
+
+    next();
+  })
   .use('/api/v1', apiRouter)
 
 const webServer = app.listen(80)
