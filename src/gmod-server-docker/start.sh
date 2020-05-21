5141efd68373d22df422ad0ebdee8a324c694d5c
Leondro Lio <github@leondrolio.com>
2020-05-21 06:38:53 +0100

    Test use of `exec` to run srcds

--- src/gmod-server-docker/start.sh
+++ src/gmod-server-docker/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-faketty () {
-  script -qfc "$(printf "'%s' " "$@")"
-}
+# faketty () {
+#   script -qfc "$(printf "'%s' " "$@")"
+# }
 
 unionfs-fuse -o cow /gmod/write=RW:/gmod/instance=RO:/gmod/common=RO:/gmod/base=RO /gmod/union
-faketty ./union/srcds_run -console -game garrysmod -norestart +exec "server.cfg" -port ${PORT} +maxplayers ${MAXPLAYERS} +hostname "${G_HOSTNAME}" +gamemode ${GAMEMODE} "${ARGS}" +map ${MAP}
+exec ./union/srcds_run -console -game garrysmod -norestart +exec "server.cfg" -port ${PORT} +maxplayers ${MAXPLAYERS} +hostname "${G_HOSTNAME}" +gamemode ${GAMEMODE} "${ARGS}" +map ${MAP}
