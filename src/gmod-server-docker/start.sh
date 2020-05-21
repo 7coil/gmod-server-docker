5e3835a013e28b13ac11f0713813c06f152cc845
Leondro Lio <github@leondrolio.com>
2020-05-21 05:54:44 +0100

    Add third layer to allow for commiting cfg to GitHub

--- src/gmod-server-docker/start.sh
+++ src/gmod-server-docker/start.sh
@@ -4,5 +4,5 @@ faketty () {
   script -qfc "$(printf "'%s' " "$@")"
 }
 
-unionfs-fuse -o cow /gmod/specific=RW:/gmod/common=RO:/gmod/base=RO /gmod/union
+unionfs-fuse -o cow /gmod/write=RW:/gmod/instance=RO:/gmod/common=RO:/gmod/base=RO /gmod/union
 faketty ./union/srcds_run -console -game garrysmod -norestart +exec "server.cfg" -port ${PORT} +maxplayers ${MAXPLAYERS} +hostname "${G_HOSTNAME}" +gamemode ${GAMEMODE} "${ARGS}" +map ${MAP}
