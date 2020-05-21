b847be982003f1483f9a8cd8a99265450a7620d8
Leondro Lio <github@leondrolio.com>
2020-05-21 16:55:51 +0100

    Use a fake tty within docker-compose.yml

--- src/gmod-server-docker/start.sh
+++ src/gmod-server-docker/start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-# faketty () {
-#   script -qfc "$(printf "'%s' " "$@")"
-# }
-
 unionfs-fuse -o cow /gmod/write=RW:/gmod/instance=RO:/gmod/common=RO:/gmod/base=RO /gmod/union
 exec ./union/srcds_run -console -game garrysmod -norestart +exec "server.cfg" -port ${PORT} +maxplayers ${MAXPLAYERS} +hostname "${G_HOSTNAME}" +gamemode ${GAMEMODE} "${ARGS}" +map ${MAP}
